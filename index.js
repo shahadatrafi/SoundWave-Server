37a3eba4f3470de9b91923d8b4073401e5712b10
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-06-14 05:13:50 +0600

    class denied status api added

--- index.js
+++ index.js
@@ -164,7 +164,18 @@ async function run() {
       res.send(result);
     })
     
-    
+    app.put('/classes/denied/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          status: 'denied'
+        },
+      };
+      const result = await classesCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
 
     // carts api
     app.get('/carts', verifyJWT, async (req, res) => {
